161a0d7581f135cb7724baec94cbbffefe2b30ab
Tarun Vadde <142969534+tarunvadde@users.noreply.github.com>
2026-08-25 02:55:30 +0530

    feat: visual graph explorer at /explorer (#26)
    Signed-off-by: Tarun Vadde <vaddeofficial@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify'
 import { registerDocs } from './api/docs'
 import { apiErrorHandler } from './api/errors'
+import { registerExplorer } from './api/explorer'
 import { registerSearchRoute } from './api/search/route'
 import { registerTraverseRoute } from './api/traverse/route'
 import { attachBlockProgressServer } from './bps/server'
@@ -36,6 +37,7 @@ app.get('/health', async () => ({
   lastAppliedBlock: orchestrator.currentState.lastAppliedBlock,
 }))
 registerDocs(app)
+registerExplorer(app)
 registerTraverseRoute(app, db)
 registerSearchRoute(app, db, config)
 
